909ec0d1aa4ecb2e6f045f6b3135680afb91c445
Aaron-Shrike <aaronrv138@gmail.com>
2022-03-30 00:23:28 -0500

    server.js - Despliegue

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.get(/.*/, function (req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
 })
-const port = precess.env.PORT || 8080
+const port = process.env.PORT || 8080
 app.listen(port)
 console.log(`El sitio esta listo en el puerto: ${port}`)
